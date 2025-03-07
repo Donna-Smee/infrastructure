71b87f782b7d48337806ae6ebc8dd35c02cfb91e
Donna-Smee <dailydosesofdonuts@gmail.com>
2025-03-07 18:27:08 -0500

    delete extra .tf files for actions to work

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudinit" {
+  # Configuration options
+}
+
+variable "region" {
+  type        = string
+  default     = "Canada Central"
+  description = "The region that the resources will be in"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "CST8918-Lab9-RG"
   location = var.region
